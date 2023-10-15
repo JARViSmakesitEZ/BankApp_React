2e69733a804ae79d90ae663541ad55571f0a0970
JARViSmakesitEZ <103288712+JARViSmakesitEZ@users.noreply.github.com>
2023-10-15 21:34:16 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navtext from "./Navtext";
+
+function Navbar(props) {
+  return (
+    <nav>
+      <Navtext text="Welcome to LIB ." />
+      <div id="logo">
+        <img src="/assets/logo.png" alt="bankLogo" />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+// const newStyle = {
+//   color: "red",
+//   fontSize: "20px",
+//   border: "10px solid black",
+// };
+ReactDOM.render(<App />, document.getElementById("root"));
